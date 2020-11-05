a27af9aaffd7fd6b95c8b04b5e7d29e7fc5b380c
terry-xiaoyu <506895667@qq.com>
2020-11-05 17:20:19 +0800

    featrue(funcs): add sql func mget/2,3 and mput/3
    The map_get/2,3 and map_put/3 support nested get/put but it is wrong by design, for example:
    
    ```
    map_get('a.b', payload)
    ```
    
    This could not distinguish the following two payloads:
    
    ```json
    {"a": {"b": 1}}
    ```
    
    and
    
    ```json
    {"a.b": 1}
    ```
    
    So for backward compatibility I'd like to create 2 more functions to fix this issue: mget/2,3 and mput/3.

--- test/emqx_rule_funcs_SUITE.erl
+++ test/emqx_rule_funcs_SUITE.erl
@@ -499,6 +499,17 @@ t_map_put(_) ->
     ?assertEqual(#{<<"a">> => #{<<"b">> => 1}}, apply_func(map_put, [<<"a.b">>, 1, #{}])),
     ?assertEqual(#{a => #{b => 1, <<"c">> => 1}}, apply_func(map_put, [<<"a.c">>, 1, #{a => #{b => 1}}])).
 
+ t_mget(_) ->
+    ?assertEqual(1, apply_func(map_get, [<<"a">>, #{a => 1}])),
+    ?assertEqual(1, apply_func(map_get, [<<"a">>, #{<<"a">> => 1}])),
+    ?assertEqual(undefined, apply_func(map_get, [<<"a">>, #{}])).
+
+ t_mput(_) ->
+    ?assertEqual(#{<<"a">> => 1}, apply_func(map_put, [<<"a">>, 1, #{}])),
+    ?assertEqual(#{<<"a">> => 2}, apply_func(map_put, [<<"a">>, 2, #{<<"a">> => 1}])),
+    ?assertEqual(#{a => 2}, apply_func(map_put, [<<"a">>, 2, #{a => 1}])).
+
+
 %%------------------------------------------------------------------------------
 %% Test cases for Hash funcs
 %%------------------------------------------------------------------------------
